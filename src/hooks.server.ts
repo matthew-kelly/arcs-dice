b1ff0c7737f0aed897621ba69627cee1e1a0540b
Matt Kelly <bmatthewkelly@gmail.com>
2024-06-24 12:51:23 -0700

    add sentry monitoring

---
+++ src/hooks.server.ts
@@ -0,0 +1,17 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://abcb3d99a3dc308f21d84caf4bfd19ce@o4507488567492608.ingest.us.sentry.io/4507488572276736',
+  tracesSampleRate: 1.0,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: import.meta.env.DEV,  
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();

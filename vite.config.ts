605c12b3223391e010f5437210b6300e95928aad
Matt Kelly <bmatthewkelly@gmail.com>
2024-09-11 15:52:03 -0700

    remove sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'matthew-kelly',
-				project: 'arcs-dice',
-			},
-		}),
-		sveltekit(),
-	],
+	plugins: [sveltekit()],
 });
